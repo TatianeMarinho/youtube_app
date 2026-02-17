75af3d861a3c30d869cfc34776ab4ce9b55799a5
Tatiane Marinho <devtatianemarinho@gmail.com>
2026-02-16 20:10:08 -0400

    criando o esqueleto do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_app/routes/app_routes.dart';
+import 'package:youtube_app/theme/app_colors.dart';
 
 void main() => runApp(const MyStreamApp());
 
@@ -12,6 +13,16 @@ class MyStreamApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'My Stream Youtube',
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.deepWine,
+        primaryColor: AppColors.fireRed,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.burnRed,
+          elevation: 0,
+          iconTheme: IconThemeData(color: AppColors.softLime),
+        ),
+        iconTheme: const IconThemeData(color: AppColors.softLime),
+      ),
       routerConfig: AppRoutes.router,
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset('assets/images/youtube.png', width: 98, height: 22),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('acao: videocam');
+            },
+            icon: Icon(Icons.videocam),
+          ),
+          IconButton(
+            onPressed: () {
+              print('acao: pesquisa');
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              print('acao: conta');
+            },
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
